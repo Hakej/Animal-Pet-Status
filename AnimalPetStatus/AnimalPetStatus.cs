721102e6c47dbddfc13cf3e7207c8f7ac78bc663
Hakej <michaluszak97@gmail.com>
2021-05-10 17:20:08 +0200

    Add a Drawer with example strings drawn on screen

--- AnimalPetStatus/AnimalPetStatus.cs
+++ AnimalPetStatus/AnimalPetStatus.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewModdingAPI.Utilities;
@@ -8,10 +9,35 @@ using StardewValley;
 namespace AnimalPetStatus
 {
     public class AnimalPetStatus : Mod
-    { 
+    {
+        public bool Show = true;
+        public SButton ToggleButton = SButton.P;
+
+        // TEXT
+        public Vector2 Position = new Vector2(10, 10);
+
+        public Drawer Drawer;
+
         public override void Entry(IModHelper helper)
         {
             helper.Events.Input.ButtonPressed += OnButtonPressed;
+            helper.Events.Display.RenderedHud += OnRenderedHud;
+            helper.Events.GameLoop.GameLaunched += GameLaunched;
+        }
+
+        private void GameLaunched(object sender, GameLaunchedEventArgs e)
+        {
+            Drawer = new Drawer(Game1.spriteBatch, Game1.smallFont);
+        }
+
+        private void OnRenderedHud(object sender, RenderedHudEventArgs e)
+        {
+            if (!Show)
+                return;
+
+            string[] strings = { "One", "Two", "Three" };
+
+            Drawer.DrawStrings(strings, Position);
         }
 
         private void OnButtonPressed(object sender, ButtonPressedEventArgs e)
@@ -19,7 +45,9 @@ namespace AnimalPetStatus
             if (!Context.IsWorldReady)
                 return;
 
-            // print button presses to the console window
+            if (e.Button == ToggleButton)
+                Show = !Show;
+
             this.Monitor.Log($"{Game1.player.Name} pressed {e.Button}.", LogLevel.Debug);
         }
     }
---
+++ AnimalPetStatus/Drawer.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalPetStatus
+{
+    public class Drawer
+    {
+        private static SpriteBatch _spriteBatch;
+        private static SpriteFont _spriteFont;
+
+        private const int MULTIPLE_STRINGS_OFFSET = 25;
+
+        public Drawer(SpriteBatch spriteBatch, SpriteFont spriteFont)
+        {
+            _spriteBatch = spriteBatch;
+            _spriteFont = spriteFont;
+        }
+
+        public void DrawString(string stringToDraw, Vector2 position)
+        {
+            _spriteBatch.DrawString(_spriteFont, stringToDraw, position, Color.White);
+        }
+
+        public void DrawStrings(string[] stringsToDraw, Vector2 position)
+        {
+            var localPosition = position;
+
+            foreach (var s in stringsToDraw)
+            {
+                _spriteBatch.DrawString(_spriteFont, s, localPosition, Color.White);
+
+                localPosition.Y += MULTIPLE_STRINGS_OFFSET;
+            }
+        }
+    }
+}
